b7615cd947686df5d5eed1a664be7029ed5dfc05
Caetano Cesar <caetanocesar35@gmail.com>
2023-07-17 12:57:11 -0300

    configurando limite das posições da mosca

--- jogo.js
+++ jogo.js
@@ -12,7 +12,31 @@ function ajustaTamanhoPalcoJogo(){
 	
 ajustaTamanhoPalcoJogo()
 
-var posicaoX= Math.floor(Math.random() * largura)
-var posicaoY= Math.floor(Math.random() * altura)
+function posicaoRandomica(){
+
+var posicaoX= Math.floor(Math.random() * largura) - 90
+var posicaoY= Math.floor(Math.random() * altura) - 90
+
+if(posicaoX<0){
+	posicaoX=0
+}
+
+if(posicaoY<0){
+	posicaoY=0
+}
 
 console.log(posicaoX, posicaoY)
+
+//criar o elemento html
+var mosquito = document.createElement('img')
+mosquito.src = 'imagens/mosquito.png'
+mosquito.className = 'mosquito1'
+mosquito.style.left = posicaoX + 'px'
+mosquito.style.top = posicaoY + 'px'
+mosquito.style.position = 'absolute'
+
+
+document.body.appendChild(mosquito)
+
+}
+
